b9b6f270fc46a80b37674a8b18f7132bec92d4ca
Tetiana <tatianasurkovaa@gmail.com>
2023-04-13 23:10:19 +0300

    Add MainTitle to App

---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+import MainTitle from '../mainTitle/MainTitle'
+
+function App() {
+
+    return (
+        <div className="App">
+            <main>
+                <MainTitle>
+                    Organize. Streamline. Thrive.
+                </MainTitle>
+            </main>
+        </div>
+    )
+}
+
+export default App

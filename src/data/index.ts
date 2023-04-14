3aac1fa02ea33def4618cab0fd7e4c11cf869f7e
Tetiana <tatianasurkovaa@gmail.com>
2023-04-14 22:07:17 +0300

    Add data dir

---
+++ src/data/index.ts
@@ -0,0 +1,32 @@
+export const features = [
+    {
+        id: 1,
+        title: 'Intuitive Interface',
+        desc: 'Our CRM is designed to be easy to use, with an intuitive interface that anyone can navigate.',
+    },
+    {
+        id: 2,
+        title: 'Advanced Reporting ',
+        desc: ' With our CRM, you can generate advanced reports that give you insight into your business like never before.',
+    },
+    {
+        id: 3,
+        title: 'Task Management',
+        desc: 'Stay on top of your tasks with our built-in task management system, which allows you to assign tasks, set due dates, and track progress.',
+    },
+    {
+        id: 4,
+        title: 'Customizable',
+        desc: 'Our CRM is fully customizable to fit your unique business needs. From custom fields to custom workflows, you can tailor our CRM to your specific requirements.',
+    },
+    {
+        id: 5,
+        title: 'Mobile App',
+        desc: 'Stay connected on the go with our mobile app, which allows you to access your CRM from anywhere, at any time.',
+    },
+    {
+        id: 6,
+        title: 'Customer Support ',
+        desc: 'Our dedicated support team is here to help you with any questions or issues you may have, ensuring that you get the most out of our CRM.',
+    },
+]

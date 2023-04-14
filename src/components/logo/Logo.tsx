02a64f1002a8db4cb063528da58670e401113595
Tetiana <tatianasurkovaa@gmail.com>
2023-04-14 22:07:40 +0300

    Create logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,16 @@
+import './Logo.scss'
+import { LogoIcon } from '../../assets'
+
+function Logo(): JSX.Element {
+    return (
+        <div className='logo'>
+            <LogoIcon className='logo__icon'/>
+            <span className="logo__text">
+                ThriveTech
+            </span>
+
+        </div>
+    )
+}
+
+export default Logo

5a5a55f4028d074d3ca873f3fac79860d2c95b62
Tetiana <tatianasurkovaa@gmail.com>
2023-04-18 01:16:11 +0300

    Create section title component

---
+++ src/components/ui/sectionTitle/SectionTitle.tsx
@@ -0,0 +1,28 @@
+import './SectionTitle.scss'
+import { CSSProperties } from 'react'
+
+interface SectionTitleProps {
+    children: String
+    colors?: { text: String, first: String, second: String }
+}
+
+const defaultColors = {
+    text: '#9ec0d5', first: '#00DFD8', second: '#007CF0',
+}
+
+function SectionTitle({ children, colors = defaultColors }: SectionTitleProps): JSX.Element {
+    const styleColors = {
+        '--text-color': colors?.text,
+        '--first-color': colors?.first,
+        '--second-color': colors?.second,
+    }
+
+    return (
+        <h2 className="section-title" style={styleColors as CSSProperties}>
+            {children}
+            <span/>
+        </h2>
+    )
+}
+
+export default SectionTitle

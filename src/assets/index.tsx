63f4622ea103297a8a176c8e1277a073548b0923
Tetiana <tatianasurkovaa@gmail.com>
2023-04-21 20:11:23 +0300

    Add icons

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,1 +1,4 @@
 export { ReactComponent as LogoIcon } from './logo.svg'
+export { ReactComponent as SunIcon } from './sun.svg'
+export { ReactComponent as MoonIcon } from './moon.svg'
+export { ReactComponent as MonitorIcon } from './monitor.svg'

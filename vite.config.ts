b77476f6a7e9332bb734788992770e0219517c2c
Tetiana <tatianasurkovaa@gmail.com>
2023-04-21 20:14:29 +0300

    Add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,13 @@ import path from 'path'
 export default defineConfig({
     resolve: {
         alias: {
-
             '@assets': path.resolve(__dirname, './src/assets'),
             '@components': path.resolve(__dirname, './src/components'),
+            '@constants': path.resolve(__dirname, './src/constants'),
+            '@contexts': path.resolve(__dirname, './src/contexts'),
             '@data': path.resolve(__dirname, './src/data'),
+            '@helpers': path.resolve(__dirname, './src/helpers'),
+            '@/types': path.resolve(__dirname, './src/types'),
         },
     },
     plugins: [ react(), svgr() ],

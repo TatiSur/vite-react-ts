a30be23cdd1630934646c88b24df104b43fcbea4
Tetiana <tatianasurkovaa@gmail.com>
2023-04-21 20:16:48 +0300

    Update Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,41 +1,32 @@
 import './Navbar.scss'
 import Logo from '@components/ui/logo/Logo'
 import Button from '@components/ui/button/Button'
+import { navLinks } from '@constants/index'
+import ThemeSwitcher from '@components/ui/themeSwitcher/ThemeSwitcher'
 
 function Navbar(): JSX.Element {
 
     return (
         <div className="navbar">
             <Logo/>
-
             <nav>
                 <ul className="navbar__list">
-                    <li>
-                        <a href="#features">
-                            Features
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#pricing">
-                            Pricing
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#testimonials">
-                            Testimonials
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            Contact Us
-                        </a>
-                    </li>
+                    {navLinks.map(link => (
+                        <li key={link.id}>
+                            <a href={link.href}>
+                                {link.name}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </nav>
 
-            <Button size="small">
-                Sign in
-            </Button>
+            <div className='navbar__buttons'>
+                <ThemeSwitcher/>
+                <Button size="small">
+                    Sign in
+                </Button>
+            </div>
         </div>
     )
 }
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+        id: 1,
+        name: 'Features',
+        href: '#features'
+    },
+    {
+        id: 2,
+        name: 'Pricing',
+        href: '#pricing'
+    },
+    {
+        id: 3,
+        name: 'Testimonials',
+        href: '#testimonials'
+    },
+    {
+        id: 4,
+        name: 'Contact Us',
+        href: '#contact-us'
+    },
+]

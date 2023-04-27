173d44ced3f18d8235f241a9aef06cc3a1668fe3
Tetiana <tatianasurkovaa@gmail.com>
2023-04-27 22:43:54 +0300

    Add to app ContactUsSection

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,9 @@
 import './App.scss'
-import MainScreen from '@components/mainScreen/MainScreen'
+import MainScreen from '@components/section/mainScreen/MainScreen'
 import FeaturesSection from '@components/section/featuresSection/FeaturesSection'
 import PricingScreen from '@components/section/pricingSection/PricingScreen'
 import TestimonialsSection from '@components/section/testimonialsSection/TestimonialsSection'
+import ContactUsSection from '@components/section/contactUsSection/ContactUsSection'
 import Footer from '@components/footer/Footer'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
                 <FeaturesSection/>
                 <PricingScreen/>
                 <TestimonialsSection/>
+                <ContactUsSection/>
             </main>
             <Footer/>
         </>

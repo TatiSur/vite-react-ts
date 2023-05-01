8c5c74c7c899d03588abc867102ed08137bdbfea
Tetiana <tatianasurkovaa@gmail.com>
2023-05-01 23:01:58 +0300

    Update Input

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,17 +1,43 @@
-import { FC, InputHTMLAttributes, ReactNode } from 'react'
+import { forwardRef, InputHTMLAttributes, ReactNode } from 'react'
 import './Input.scss'
+import { ErrorOutlineRoundedIcon } from '@assets/index'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string
+    error?: string
     icon?: ReactNode;
 }
 
-const Input: FC<InputProps> = ({ icon, type = 'text', ...rest }) => {
+const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+    const { label, error, icon, type = 'text', maxLength = 25, ...rest } = props
+
     return (
-        <div className="input-container">
+        <div className={`input input${error ? '--error' : ''}`}>
+            {label && <label className="input__label">{label}</label>}
             {icon && <div className="icon">{icon}</div>}
-            <input type={type} {...rest} />
+            <input
+                className="input__field"
+                ref={ref}
+                type={type}
+                {...rest}
+            />
+            {error && (
+                <>
+                    <ErrorOutlineRoundedIcon
+                        className="input__error-icon"
+                        data-tooltip-id="input-error"
+                        data-tooltip-content={error}
+                    />
+                    <ReactTooltip
+                        id="input-error"
+                        place="left"
+                        variant="error"
+                    />
+                </>
+            )}
         </div>
     )
-}
+})
 
 export default Input
